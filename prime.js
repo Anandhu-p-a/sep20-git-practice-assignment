6a4293467241956b8721d785d005040944cccfc9
Anandhu__P_A <anandhupa.in@gmail.com>
2022-09-20 13:58:08 +0530

    version two

--- prime.js
+++ prime.js
@@ -5,5 +5,7 @@ var flag =true;
 for(var i=2;i<num;i++){
     if(i%num==0){flag =false}
 }
-if(flag){console.log("Not prime")}
+if(flag){console.log("prime")}
 else{console.log("Not prime")}
+//version two by myself
+
